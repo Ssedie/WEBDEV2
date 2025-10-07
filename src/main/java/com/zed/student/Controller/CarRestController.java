fd75ccd510b0f0e5665bf8713940eb39cf62c1be
Ssedie <zedric.rulloda@lorma.edu>
2025-10-07 19:03:10 +0800

    pwede na siguro toh noh??

--- src/main/java/com/zed/student/Controller/CarRestController.java
+++ src/main/java/com/zed/student/Controller/CarRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@CrossOrigin
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 public class CarRestController {
